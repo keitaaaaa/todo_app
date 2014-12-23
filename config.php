6a7e954ebf5337c2388fff7103f3be12cc273e94
keitaaaaa <wkeitaorange@gmail.com>
2014-12-23 14:21:05 +0900

    clearDBの設定

--- config.php
+++ config.php
@@ -2,10 +2,19 @@
 
 // DB
 
-define('DSN', 'mysql:host=localhost;dbname=todo_app;charset=utf8;');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DSN', 'mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_1d771801b9c601c;charset=utf8;');
+define('DB_USER', 'bc28633116647f');
+define('DB_PASSWORD', '04781d6e');
 
 // エラー表示
 
 error_reporting(E_ALL & ~E_NOTICE);
+
+
+
+
+
+// bc28633116647f
+// 04781d6e
+// us-cdbr-iron-east-01.cleardb.net
+// heroku_1d771801b9c601c
